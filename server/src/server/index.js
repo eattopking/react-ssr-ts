f8c701ac8b1f65fa60defbff77809ad4c91918a7
zhanghengh <zhanghengh@yonyou.com>
2019-07-31 23:06:45 +0800

    前后端同构成功

--- server/src/server/index.js
+++ server/src/server/index.js
@@ -2,20 +2,20 @@ import Koa from "koa";
 import React from "react";
 import { renderToString } from "react-dom/server";
 import Table from "../containers/Home";
-import Static from "koa-static"; // 静态资源服务管理中间件
+import koaStatic from "koa-static"; // 静态资源服务管理中间件
 import path from "path"; // 路径管理
 const app = new Koa();
 const content = renderToString(<Table />);
 
-
-// 设置静态资源路径, 当客户端请求静态资源是,就到对应目录下寻找返回
-app.use(Static(path.resolve(__dirname, "../public")));
+// 设置静态资源路径, 当客户端请求静态资源是,就到对应目录下寻找返回,
+// 这里直接设置项目目录下的目录名就行,直接koa就能找到,不用整那些乱七八糟的
+app.use(koaStatic("public"));
 app.use(async ctx => {
   ctx.body = `<!DOCTYPE html>
   <html lang="zh-CN">
     <head>
       <title>ssrTable</title>
-      <link rel="stylesheet" href="">
+      
     </head>
     <body>
       <div id="app">${content}</div>
