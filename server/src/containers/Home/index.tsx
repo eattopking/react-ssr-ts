faaf86c49c0a38a13b3b0fa18c9c68f1703b5bf2
zhanghengh <zhanghengh@yonyou.com>
2019-08-13 23:37:25 +0800

    优化逻辑代码,实现可配置

--- server/src/containers/Home/index.tsx
+++ server/src/containers/Home/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Layout, Menu } from "antd";
-import { Link } from "react-router-dom";
-import Routes from "./routes";
+import { Link, Route } from "react-router-dom";
+import routes from "./routes";
 const { Content, Footer, Sider } = Layout;
 
 export default function PageLayout() {
@@ -11,12 +11,18 @@ export default function PageLayout() {
         <Layout style={{ padding: "24px 0", background: "#fff" }}>
           <Sider width={200} style={{ background: "#fff" }}>
             <Menu mode="inline" defaultSelectedKeys={["1"]} style={{ height: "100%" }}>
-              <Menu.Item key="1"><Link to="/">simpletable</Link></Menu.Item>
-              <Menu.Item key="2"><Link to="/difficult">difftable</Link></Menu.Item>
+              <Menu.Item key="1">
+                <Link to="/">simpletable</Link>
+              </Menu.Item>
+              <Menu.Item key="2">
+                <Link to="/diff">difftable</Link>
+              </Menu.Item>
             </Menu>
           </Sider>
           <Content style={{ padding: "0 24px", minHeight: 280 }}>
-            <Routes />
+            {routes.map(routeConfig => {
+              return <Route {...routeConfig} />;
+            })}
           </Content>
         </Layout>
       </Content>
--- server/src/containers/Home/routes.tsx
+++ server/src/containers/Home/routes.tsx
@@ -1,13 +1,17 @@
-import React from "react";
-import { Route } from "react-router-dom";
 import simpleTable from "./components/simple_table";
 import diffTable from "./components/diff_table";
 
-export default function Routes() {
-  return (
-    <>
-      <Route exact path="/" component={simpleTable} />
-      <Route path="/difficult" component={diffTable} />
-    </>
-  );
-}
+export default [
+  {
+    path: "/",
+    exact: true,
+    component: simpleTable,
+    key: "simple"
+  },
+  {
+    path: "/diff",
+    exact: true,
+    component: diffTable,
+    key: "diff"
+  }
+];
