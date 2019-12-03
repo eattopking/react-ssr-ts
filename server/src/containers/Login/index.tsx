1f11b4bc17fd585b996d6476a0b1d5d69f357cc3
zhanghengh <zhanghengh@yonyou.com>
2019-12-03 23:56:41 +0800

    优化了两个页面的样式

--- server/src/containers/Login/index.tsx
+++ server/src/containers/Login/index.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { Layout, Input, Button, Form, message } from "antd";
-
+const { Item } = Form;
 const { Content, Footer } = Layout;
 const axios = require("axios");
 
-function Login({ form }: { form: { getFieldDecorator: Function; validateFields: Function } }) {
+/**
+ * 登录页组件
+ * @param form 高阶组件包裹后,传递过来的form方法
+ */
+function Login({
+  form
+}: {
+  form: { getFieldDecorator: Function; validateFields: Function };
+}) {
   const { getFieldDecorator, validateFields } = form;
   const handleSubmit = (e: { preventDefault: Function }) => {
     e.preventDefault();
@@ -15,7 +23,7 @@ function Login({ form }: { form: { getFieldDecorator: Function; validateFields: 
             data: { status }
           } = res;
           if (status) {
-            window.location.href = "http://eattopking.top:8000/page";
+            window.location.href = "http://localhost:8000/page";
           } else {
             message.info("用户名或密码不正确,请重新输入");
           }
@@ -31,27 +39,53 @@ function Login({ form }: { form: { getFieldDecorator: Function; validateFields: 
       <Content style={{ padding: "0 50px" }}>
         <Layout style={{ padding: "24px 0", background: "#fff" }}>
           <Content style={{ padding: "0 24px", minHeight: 280 }}>
-            <Form onSubmit={handleSubmit} className="login-form">
-              <Form.Item>
+            <Form onSubmit={handleSubmit}>
+              <Item
+                style={{ display: "flex", justifyContent: "center" }}
+                label="邮箱"
+              >
                 {getFieldDecorator("mail", {
-                  rules: [{ required: true, message: "Please input your username!" }]
-                })(<Input placeholder="用户" />)}
-              </Form.Item>
-              <Form.Item>
+                  rules: [
+                    { required: true, message: "Please input your username!" }
+                  ]
+                })(<Input style={{ width: 200 }} placeholder="用户" />)}
+              </Item>
+              <Item
+                style={{ display: "flex", justifyContent: "center" }}
+                label="密码"
+              >
                 {getFieldDecorator("password", {
-                  rules: [{ required: true, message: "Please input your Password!" }]
-                })(<Input type="password" placeholder="密码" />)}
-              </Form.Item>
-              <Form.Item>
-                <Button type="primary" htmlType="submit" className="login-form-button">
-                  Log in
+                  rules: [
+                    { required: true, message: "Please input your Password!" }
+                  ]
+                })(
+                  <Input
+                    style={{ width: 200 }}
+                    type="password"
+                    placeholder="密码"
+                  />
+                )}
+              </Item>
+              <Item style={{ display: "flex", justifyContent: "center" }}>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  className="login-form-button"
+                >
+                  登录
                 </Button>
-              </Form.Item>
+                <div
+                  style={{ color: "red", textAlign: "center" }}
+                  onClick={handleRegister}
+                >
+                  去注册
+                </div>
+              </Item>
             </Form>
           </Content>
         </Layout>
       </Content>
-      <div onClick={handleRegister}>注册</div>
+
       <Footer style={{ textAlign: "center" }}>登录界面</Footer>
     </Layout>
   );
--- server/src/containers/Register/index.tsx
+++ server/src/containers/Register/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Layout, Input, Button, Form, message } from "antd";
-
+const { Item } = Form;
 const { Content, Footer } = Layout;
 const axios = require("axios");
 
-function Register({ form }: { form: { getFieldDecorator: Function; validateFields: Function } }) {
+function Register({
+  form
+}: {
+  form: { getFieldDecorator: Function; validateFields: Function };
+}) {
   const { getFieldDecorator, validateFields } = form;
   const handleSubmit = (e: { preventDefault: Function }) => {
     e.preventDefault();
@@ -31,21 +35,35 @@ function Register({ form }: { form: { getFieldDecorator: Function; validateField
         <Layout style={{ padding: "24px 0", background: "#fff" }}>
           <Content style={{ padding: "0 24px", minHeight: 280 }}>
             <Form onSubmit={handleSubmit} className="login-form">
-              <Form.Item>
+              <Item
+                label="邮箱"
+                style={{ display: "flex", justifyContent: "center" }}
+              >
                 {getFieldDecorator("mail", {
-                  rules: [{ required: true, message: "Please input your username!" }]
+                  rules: [
+                    { required: true, message: "Please input your username!" }
+                  ]
                 })(<Input placeholder="用户" />)}
-              </Form.Item>
-              <Form.Item>
+              </Item>
+              <Item
+                label="密码"
+                style={{ display: "flex", justifyContent: "center" }}
+              >
                 {getFieldDecorator("password", {
-                  rules: [{ required: true, message: "Please input your Password!" }]
+                  rules: [
+                    { required: true, message: "Please input your Password!" }
+                  ]
                 })(<Input type="password" placeholder="密码" />)}
-              </Form.Item>
-              <Form.Item>
-                <Button type="primary" htmlType="submit" className="login-form-button">
-                  Register
+              </Item>
+              <Item style={{ display: "flex", justifyContent: "center" }}>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  className="login-form-button"
+                >
+                  注册
                 </Button>
-              </Form.Item>
+              </Item>
             </Form>
           </Content>
         </Layout>
