083f82407779c27eff6ffff6d0f81cf847317bbc
zhangheng05 <zhangheng05@baijiahulian.com>
2021-07-19 20:29:00 +0800

    feat: 更新数据库配置

--- src/mysql/models/page_table.js
+++ src/mysql/models/page_table.js
@@ -8,9 +8,9 @@ const pageTable = sequelize.define(
   "pageTable",
   // 设置表字段, 并规范类型, 好像只有第一次有用,之后好像没啥用了
   {
-    key: DataTypes.STRING(100),
-    name: DataTypes.STRING(100),
-    info: DataTypes.STRING,
+    key: DataTypes.STRING(1000),
+    name: DataTypes.STRING(1000),
+    info: DataTypes.STRING(1000),
   },
   {
     // 不要默认时间戳
